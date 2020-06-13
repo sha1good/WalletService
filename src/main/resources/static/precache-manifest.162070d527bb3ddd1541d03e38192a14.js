b23c74a88c963fe209b762a053cff9fa2858f30a
sha1good <60278632+sha1good@users.noreply.github.com>
2020-06-13 18:16:00 +0100

    Adding the static file

---
+++ src/main/resources/static/precache-manifest.162070d527bb3ddd1541d03e38192a14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feefed7ac30849e88866358e6f391df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05bfb2f9e3fbe81e8e35",
+    "url": "/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "84ea88ffa692b9402e02",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05bfb2f9e3fbe81e8e35",
+    "url": "/static/js/2.30d21073.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.30d21073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ea88ffa692b9402e02",
+    "url": "/static/js/main.0384b0d8.chunk.js"
+  },
+  {
+    "revision": "45597bfba923b0acc757",
+    "url": "/static/js/runtime-main.bdcf3343.js"
+  }
+]);
